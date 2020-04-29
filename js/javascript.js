0bcdea5de431ddb69a5dd437d1f9e6306fd24b8c
Pablo Monteserín <paramonty@gmail.com>
2020-04-29 20:19:32 +0200

    primera subida

---
+++ js/javascript.js
@@ -0,0 +1,40 @@
+class Escena extends Phaser.Scene {
+
+    preload() {
+        this.load.baseURL = '/curso/phaser/ex/acierta-imagen/';
+        resize();
+        window.addEventListener('resize', resize);
+        this.load.image('fondo', 'img/fondo.jpg');
+    }
+
+    create() {
+        this.add.sprite(480, 320, 'fondo');
+    }
+}
+
+function resize() {
+    const canvas = document.querySelector("canvas");
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+
+    const windowRatio = windowWidth / windowHeight;
+    const gameRatio = config.width / config.height;
+    if (windowRatio < gameRatio) {
+        canvas.style.width = `${windowWidth}px`;
+        canvas.style.height = `${windowWidth / gameRatio}px`;
+    } else {
+        canvas.style.width = `${windowHeight * gameRatio}px`;
+        canvas.style.height = `${windowHeight}px`;
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 960,
+    height: 640,
+    scene: Escena,
+};
+
+new Phaser.Game(config);
+window.addEventListener('resize', resize, false);
+
