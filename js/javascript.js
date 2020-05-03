59a3c661aa93b97807673e1e1011df6817c0a639
Pablo Monteserín <paramonty@gmail.com>
2020-05-03 19:00:25 +0200

    corrección resize

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,7 @@
 class Escena extends Phaser.Scene {
 
     preload() {
-        resize();
-        window.addEventListener('resize', resize, false);
+        
     }
 
     create() {
@@ -10,27 +9,15 @@ class Escena extends Phaser.Scene {
     }
 }
 
-function resize() {
-    const canvas = document.querySelector("canvas");
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
-
-    const windowRatio = windowWidth / windowHeight;
-    const gameRatio = config.width / config.height;
-    if (windowRatio < gameRatio) {
-        canvas.style.width = `${windowWidth}px`;
-        canvas.style.height = `${windowWidth / gameRatio}px`;
-    } else {
-        canvas.style.width = `${windowHeight * gameRatio}px`;
-        canvas.style.height = `${windowHeight}px`;
-    }
-}
 
 const config = {
     type: Phaser.AUTO,
     width: 960,
     height: 640,
     scene: Escena,
+    scale: {
+		mode: Phaser.Scale.FIT
+	}
 };
 
 new Phaser.Game(config);
