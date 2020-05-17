1224d2dcc7c4891d80b01e02925e4880fe13dfd4
Pablo Monteserín <paramonty@gmail.com>
2020-05-17 21:04:57 +0200

    physics added

--- js/javascript.js
+++ js/javascript.js
@@ -17,7 +17,16 @@ const config = {
     scene: Escena,
     scale: {
 		mode: Phaser.Scale.FIT
-	}
+    },
+    /*physics: {
+		default: 'arcade',
+		arcade: {
+			debug: true,
+			gravity: {
+				y: 300,
+			},
+		},
+	},*/
 };
 
 new Phaser.Game(config);
